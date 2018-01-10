cfeddd42468961c377470ec9022b896799e6c0f8
Mahmoud <mayounes12345@gmil.com>
2018-01-10 18:59:43 +0200

    cards

--- This is the Game/cards.js
+++ This is the Game/cards.js
@@ -1,17 +1,27 @@
-var cards = function(checked){
-    Object.defineProperty(this,'checked',{
-        get: function () {
-            return checked
-        },
-        set:function (v) {
-            checked=v
-        }
+class cards 
+{
+    constructor(id,check,src,correct)
+    {
+        this.id=id
+        this.check=check
+        this.src=src
+        this.correct=correct
     }
-    )};
-cards.prototype.set_Img = function(src,id){
-    var f_Img=document.getElementById(id);
-    f_Img.setAttribute('src',src);
-};
-
-var c1 = new  cards('false');
-c1.set_Img('red.png','img1');
+    set_img(new_src)
+    {
+        //this.src=new_src
+        var img_id=document.getElementById(this.id)
+        img_id.setAttribute('src',new_src)
+        this.check=!this.check;
+    }
+}
+function ch_img(obj,src)
+{
+    return function()
+    {
+        obj.set_img(src);
+    }
+}
+var c1=new cards('img1',false,'green.png',false);
+var img_img1=document.getElementById('img1');
+img_img1.addEventListener('click',ch_img(c1,'red.png'));
